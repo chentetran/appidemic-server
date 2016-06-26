518a441faa03dc325064701b7704c5bd875f3e69
Vincent Tran <vincenttran23@gmail.com>
2016-06-26 17:40:41 -0400

    map inits to user's location if available

--- public/script.js
+++ public/script.js
@@ -1,42 +1,43 @@
 var lat = 0;
 var lng = 0;
-// var request = new XMLHttpRequest();
-var me = new google.maps.LatLng(lat, lng);
-var infowindow = new google.maps.InfoWindow();
+var me;
+var infowindow;
 var map;
 var marker;
 var messageData;
-var options = {
-					zoom: 15,
-					center: me,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-			  };
-			  
+var options;
+
 function init()
 {
-	getMyLocation();
-	map = new google.maps.Map(document.getElementById("map"), options);
-
-};
-
-function getMyLocation() {
+	// Get user's location
 	if (navigator.geolocation) { // if browser supported
-
 		navigator.geolocation.getCurrentPosition( 
 			function(position) {
 				lat = position.coords.latitude;
 				lng = position.coords.longitude;
+				console.log(lat);
+				console.log(lng);
+				renderMap();
 			});
 	}
 	else {
-		alert("Download Chrome.")
+		alert("Your browser does not support location");
+		renderMap();
 	}
-};
+}
+
 
 function renderMap()
 {
 	me = new google.maps.LatLng(lat, lng);
+	infowindow = new google.maps.InfoWindow();
+	options = {
+					zoom: 7,
+					center: me,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+			  };
 
+	map = new google.maps.Map(document.getElementById("map"), options);
 	map.panTo(me);
 	marker = new google.maps.Marker({
 		position: me
@@ -48,45 +49,3 @@ function renderMap()
 		infowindow.open(this.getMap(), this);
 	});
 }
-
-Number.prototype.toRad = function() {
-   return this * Math.PI / 180;
-}
-
-// function closestLandmark()
-// {
-// 	var shortest = 0;
-// 	var shortest_haversine = 1000;
-// 	for (var i = 0; i < messageData['landmarks'].length; i++) {
-// 		_lat = messageData['landmarks'][i]['geometry']['coordinates'][1];
-// 		_lng = messageData['landmarks'][i]['geometry']['coordinates'][0];
-
-// 		temp = haversine(_lat,_lng);
-// 		if (temp < shortest_haversine) {
-// 			shortest = i;
-// 			shortest_haversine = temp;
-// 		}
-// 	}
-// 	return [messageData['landmarks'][shortest], shortest_haversine]
-// }
-
-// // rounded to thousandths place
-// function haversine(destLat, destLng) {
-// 	var lat2 = destLat; 
-// 	var lon2 = destLng; 
-// 	var lat1 = lat; 
-// 	var lon1 = lng; 
-
-// 	var R = 3958.756; // miles
-// 	var x1 = lat2-lat1;
-// 	var dLat = x1.toRad();  
-// 	var x2 = lon2-lon1;
-// 	var dLon = x2.toRad();  
-// 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-// 	                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
-// 	                Math.sin(dLon/2) * Math.sin(dLon/2);  
-// 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-// 	var d = R * c; 
-
-// 	return Number((d).toFixed(3));
-// }
